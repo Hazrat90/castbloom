7319c85dd16089cdf123bc28d97f4994dafc49a6
Hazrat90 <127823732+Hazrat90@users.noreply.github.com>
2025-10-03 15:13:06 -0700

    Create main.tsx

---
+++ src/src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import CastBloom from './App'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <CastBloom />
+  </React.StrictMode>,
+)
